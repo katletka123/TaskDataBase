a211ff6e3c7d51d2ec9b313d6d7b767073473fff
katletka123 <nkoslova2000@gmail.com>
2026-02-03 16:19:40 +0100

    Add Select

--- main.py
+++ main.py
@@ -11,37 +11,6 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 
-#student table
-with open("students.json", "r", encoding="utf-8") as f:
-    data = json.load(f)
-
-create_table_query="""
-CREATE TABLE IF NOT EXISTS students (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    birthday TIMESTAMP NOT NULL,
-    room INT,
-    sex CHAR(1) CHECK (sex IN ('M', 'F'))
-);
-
-"""
-cur.execute(create_table_query)
-
-insert_query = """
-INSERT INTO students (id, name, birthday, room, sex)
-VALUES (%s, %s, %s, %s, %s)
-ON CONFLICT (id) DO NOTHING;
-"""
-
-for student in data:
-    cur.execute(insert_query, (
-        student["id"],
-        student["name"],
-        student["birthday"],
-        student["room"],
-        student["sex"]
-    ))
-
 
 #room table
 with open("rooms.json", "r", encoding="utf-8") as f:
@@ -66,6 +35,52 @@ for room in data_rooms:
         room["name"]
     ))
 
+
+
+#student table
+with open("students.json", "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+create_table_query="""
+CREATE TABLE IF NOT EXISTS students (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    birthday TIMESTAMP NOT NULL,
+    room INT REFERENCES rooms(id),
+    sex CHAR(1) CHECK (sex IN ('M', 'F'))
+);
+
+"""
+cur.execute(create_table_query)
+
+insert_query = """
+INSERT INTO students (id, name, birthday, room, sex)
+VALUES (%s, %s, %s, %s, %s)
+ON CONFLICT (id) DO NOTHING;
+"""
+
+for student in data:
+    cur.execute(insert_query, (
+        student["id"],
+        student["name"],
+        student["birthday"],
+        student["room"],
+        student["sex"]
+    ))
+
+select_query= """
+SELECT
+    rooms.id,
+    COUNT(students.name) AS students_count
+FROM rooms
+LEFT JOIN students ON rooms.id = students.room
+GROUP BY rooms.id;
+"""
+cur.execute(select_query)
+
+results = cur.fetchall()
+for row in results:
+    print(row)
 
 conn.commit()
 cur.close()
